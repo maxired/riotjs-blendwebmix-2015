b66a7874a70c68ee0004c17837be48fa0ddffbf2
Maxired (Maxence Dalmais) <github@maxired.fr>
2015-10-26 22:33:14 +0100

    initial commit

---
+++ js/slideListener.js
@@ -0,0 +1,26 @@
+function SlideIndex () {
+  riot.observable(this)
+
+  var slideIndex = 0
+
+  var self=this;
+  this.on('start', function (index) {
+    slideIndex = index || slideIndex
+
+    document.body.onkeyup = function (el) {
+      var newIndex = slideIndex
+      if (el.keyCode === 37) {
+        newIndex = Math.max(0, slideIndex - 1)
+      } else if (el.keyCode === 39) {
+        newIndex = slideIndex + 1
+      }
+
+      if (newIndex !== slideIndex) {
+        slideIndex = newIndex
+        self.trigger('slideChange', { index: newIndex })
+      }
+    }
+
+    this.trigger('slideChange', { index: slideIndex })
+  })
+}
